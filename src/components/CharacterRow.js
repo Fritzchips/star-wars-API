e68f300f58de342a64c583d214fa0324f68efb52
Fritzchips <fritzjohn_solibio@yahoo.ca>
2021-06-14 18:09:44 -0700

    added README

--- src/components/CharacterRow.js
+++ src/components/CharacterRow.js
@@ -3,22 +3,32 @@ import { libraryContext } from "../App";
 
 function CharacterRow() {
   const library = useContext(libraryContext);
-  return (
-    <>
-      {library.characterList.userPreview.map((character) => (
-        <React.Fragment key={character.name}>
-          <tr>
-            <td>{character.name}</td>
-            <td>{character.birth_year}</td>
-            <td>{character.height}</td>
-            <td>{character.mass}</td>
-            <td>{character.homeworld}</td>
-            <td>{character.species}</td>
-          </tr>
-        </React.Fragment>
-      ))}
-    </>
-  );
+  if (library.characterList.userPreview.length > 1) {
+    return (
+      <>
+        {library.characterList.userPreview.map((character) => (
+          <React.Fragment key={character.name}>
+            <tr>
+              <td>{character.name}</td>
+              <td>{character.birth_year}</td>
+              <td>{character.height}</td>
+              <td>{character.mass}</td>
+              <td>{character.homeworld}</td>
+              <td>{character.species}</td>
+            </tr>
+          </React.Fragment>
+        ))}
+      </>
+    );
+  } else {
+    return (
+      <>
+        <tr>
+          <td colSpan="6">That character may exist in another universe</td>
+        </tr>
+      </>
+    );
+  }
 }
 
 export default CharacterRow;
--- src/components/CharacterTable.js
+++ src/components/CharacterTable.js
@@ -1,14 +1,10 @@
-import React, { useContext } from "react";
+import React from "react";
 import "../App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 import Table from "react-bootstrap/Table";
-import { libraryContext } from "../App";
 import CharacterRow from "./CharacterRow";
-import CharacterNotFound from "./CharacterNotFound";
 
 function CharacterTable() {
-  const library = useContext(libraryContext);
-
   return (
     <div>
       <Table variant="dark" className="text-center" bordered hover>
@@ -23,11 +19,7 @@ function CharacterTable() {
           </tr>
         </thead>
         <tbody>
-          {library.characterList.userPreview.length > 1 ? (
-            <CharacterRow />
-          ) : (
-            <CharacterNotFound />
-          )}
+          <CharacterRow />
         </tbody>
       </Table>
     </div>
